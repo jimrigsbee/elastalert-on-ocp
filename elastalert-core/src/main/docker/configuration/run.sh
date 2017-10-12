cd39c9ca06e496c8de3b9c6ecf0df0d2fed1a90c
Jim Rigsbee <jrigsbee@Gus.local>
2017-10-11 23:01:23 -0500

    fix 2 dockerfiles and run script

--- elastalert-core/src/main/docker/configuration/run.sh
+++ elastalert-core/src/main/docker/configuration/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cd $ELASTALERT_HOME \
-    && elastalert-create-index --index elastalert_status --old-index "" \
     && python -m elastalert.elastalert --verbose --rule rule-definition.yaml --start $(date --iso-8601)T00:00:00 --debug
